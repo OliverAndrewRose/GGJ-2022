dac2b2e4f233e0a37a16818cb2f00e2ef763a4ff
Oliver Andrew Rose <oliver.rose898@gmail.com>
2022-01-29 17:10:24 +0000

    Added basic player movement and animations.

---
+++ assets/entities/player/functions/movement/PlayerAnimator.gd
@@ -0,0 +1,40 @@
+extends Node
+onready var movement: PlayerMovement = get_node("../Movement") as PlayerMovement;
+onready var sprite: AnimatedSprite = get_node("../../sprite") as AnimatedSprite;
+var _current_animation: String = "default";
+
+
+func _process(delta) -> void:
+	_process_movement_animation();
+
+func _process_movement_animation() -> void:
+	var _dir: Vector2 = movement.current_direction;
+	
+	# 4-way motion.
+	if _dir == Vector2(1,0):
+		_set_player_animation("walk_right");
+	if _dir == Vector2(-1,0):
+		_set_player_animation("walk_left");
+	if _dir == Vector2(0,1):
+		_set_player_animation("walk_downward");
+	if _dir == Vector2(0,-1):
+		_set_player_animation("walk_upward");
+		
+	# Diagonal motion.
+	if _dir == Vector2(1,1):
+		_set_player_animation("walk_downward");
+	if _dir == Vector2(-1,1):
+		_set_player_animation("walk_downward");
+	if _dir == Vector2(-1,-1):
+		_set_player_animation("walk_upward");
+	if _dir == Vector2(1,-1):
+		_set_player_animation("walk_upward");
+	if _dir == Vector2(0,0):
+		_set_player_animation("default");
+
+func _set_player_animation(animation: String):
+	if animation == _current_animation:
+		return;
+	sprite.animation = animation;
+	_current_animation = animation;
+
---
+++ assets/entities/player/functions/movement/PlayerMovement.gd
@@ -0,0 +1,28 @@
+extends Node
+class_name PlayerMovement
+
+export(float) var speed: float = 5000.0;
+var current_direction: Vector2;
+onready var player_root: KinematicBody2D = get_node("../..") as KinematicBody2D;
+
+
+func _process(delta):
+	_process_movement(delta)
+	pass
+
+# Processes the movement.
+func _process_movement(delta):
+	
+	var _movement_dir = Vector2(0,0);
+	if Input.is_action_pressed("walk up"):
+		_movement_dir.y = -1.0;
+	if Input.is_action_pressed("walk down"):
+		_movement_dir.y = 1.0;
+	if Input.is_action_pressed("walk left"):
+		_movement_dir.x = -1.0;
+	if Input.is_action_pressed("walk right"):
+		_movement_dir.x = 1.0;
+		
+	current_direction = _movement_dir;
+	player_root.move_and_slide(_movement_dir * speed * delta)
+	pass
